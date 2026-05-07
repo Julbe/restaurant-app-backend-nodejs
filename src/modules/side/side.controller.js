11b323f52e9b26e33c36ea3a8372abb652aa8d17
JulioBeas <juliobeas195@gmail.com>
2026-05-07 11:53:28 -0600

    feat(sides): Update model

--- src/modules/side/side.controller.js
+++ src/modules/side/side.controller.js
@@ -3,8 +3,13 @@ import { Side } from "./side.model.js";
 
 export default class SideController extends BaseController {
     constructor() {
-        super(Side, "Side", ["name", "description"]);
+        super(
+            Side,
+            "Side",
+            ["name", "description"],
+            ["productId", "dishRef", "applicability.dishRefs", "applicability.areaMenuRefs"],
+            ["productId", "dishRef", "applicability.dishRefs", "applicability.areaMenuRefs"]
+        );
     }
 }
-
 
--- src/modules/side/side.model.js
+++ src/modules/side/side.model.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
+
 const sideSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  available: { type: Boolean, default: true },
-  value: { type: Number, default: 1 },
-  price: { type: Number, default: 0 },
-  extraPrice: { type: Number, default: 0 },
-  dishRef: { type: Schema.Types.ObjectId, ref: "Dish" },
-});
+    name: { type: String, required: true },
+    description: { type: String, default: "" },
+    available: { type: Boolean, default: true },
+    value: { type: Number, default: 1 },
+    extraPrice: { type: Number, default: 0 },
+    productId: { type: Schema.Types.ObjectId, ref: "Product", default: null, index: true },
+    sortOrder: { type: Number, default: 0 },
+}, { timestamps: true });
+
+sideSchema.index({ name: 1, available: 1 });
 
 export const Side = model("Side", sideSchema);
